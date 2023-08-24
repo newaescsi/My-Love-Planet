358f085ab0d1fa25874212ff5ade057dac40842d
newaescsi <127768559+newaescsi@users.noreply.github.com>
2023-08-24 23:00:34 +0200

    code comment

--- Client/mapapp/src/App.js
+++ Client/mapapp/src/App.js
@@ -1,7 +1,22 @@
+// In src/App.js im React-Frontend
+
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [nachricht, setNachricht] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/nachricht')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setNachricht(json.nachricht);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      Hallo Welt
+    <div>
+      <h1>{nachricht}</h1>
     </div>
   );
 }
--- Server/server.js
+++ Server/server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const api = require('./routes/api');
 const app = express();
-const port = 8080; app.use(cors()); app.use('/api', api);
+const port = 8080; app.use(cors()); 
+
+
+app.use('/api', api);
+
+
+/*
+app.get('/'): 
+Hier wird eine HTTP-GET-Anfrage auf der Wurzelroute ("/") definiert. 
+Das bedeutet, dass, wenn jemand die Haupt-URL der Website besucht (z.B. http://www.example.com/), dieser Codeblock ausgeführt wird.
+*/
 app.get('/', (req, res) => {
 res.send('Hello von Express');
 });
+
+
 app.listen(port, () => {
 console.log(`Server läuft auf Port ${port}`);
 });
