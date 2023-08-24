b0b0e32eb7280f6d73b390dfd3dcf63dfd208ba9
marcoruz <marco.ruzzo06@googlemail.com>
2023-08-24 20:15:10 +0200

    Server Kom. eingerichetet

---
+++ Server/routes/api/nachricht.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+router.get('/', (req, res) => {
+console.log('GET /api/nachricht ', req.ip);
+res.json({ nachricht: 'Hallo von der API!' });
+});
+router.post('/update', (req, res) => {
+console.log('PUT /api/nachricht/update ', req.ip);
+});
+module.exports = router
