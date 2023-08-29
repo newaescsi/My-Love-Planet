7b3dc94f063a9776569c77770c088f786c74d898
newaescsi <127768559+newaescsi@users.noreply.github.com>
2023-08-29 22:11:20 +0200

    code import comment

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,9 +4,28 @@ import {
   useNavigationType,
   useLocation,
 } from "react-router-dom";
-import LandingPage3 from "./pages/LandingPage3";
 import LandingPage2 from "./pages/LandingPage2";
+import LandingPage3 from "./pages/LandingPage3";
+import LandingPage4 from "./pages/LandingPage4";
 import { useEffect } from "react";
+
+
+/*
+Die Komponenten Routes und Route werden aus "react-router-dom" importiert. Diese Komponenten werden verwendet, um die Routing-Struktur der Anwendung zu definieren.
+*/
+/*
+Diese Imports werden verwendet, um die Routing- und Navigationsstruktur für die Anwendung festzulegen.
+*/
+
+/*
+Die Hooks useNavigationType und useLocation werden ebenfalls aus "react-router-dom" importiert. Diese Hooks liefern Informationen über Navigationsaktionen und die aktuelle Position im Routing.
+*/
+
+
+
+
+
+
 
 function App() {
   const action = useNavigationType();
@@ -28,7 +47,11 @@ function App() {
         title = "";
         metaDescription = "";
         break;
-      case "/landing-page-2":
+      case "/landing-page-3":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/landing-page-4":
         title = "";
         metaDescription = "";
         break;
@@ -50,8 +73,9 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<LandingPage3 />} />
-      <Route path="/landing-page-2" element={<LandingPage2 />} />
+      <Route path="/" element={<LandingPage2 />} />
+      <Route path="/landing-page-3" element={<LandingPage3 />} />
+      <Route path="/landing-page-4" element={<LandingPage4 />} />
     </Routes>
   );
 }
